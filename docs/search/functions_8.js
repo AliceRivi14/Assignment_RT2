12dae44ab6c563ea9d847f08d3ead305d483456d
AliceRivi14 <92019811+AliceRivi14@users.noreply.github.com>
2022-04-08 17:40:33 +0200

    Add files via upload

---
+++ docs/search/functions_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['y_41',['y',['../_m_o_d_e2_8cpp.html#a2795aa30f97a85f1d013e24f6292a51e',1,'y(0):&#160;MODE2.cpp'],['../_m_o_d_e3_8cpp.html#a2795aa30f97a85f1d013e24f6292a51e',1,'y(0):&#160;MODE3.cpp']]]
+];
---
+++ docs/search/variables_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['speedbindings_51',['speedBindings',['../_m_o_d_e2_8cpp.html#a942d4f1c3e151d6f264d0868766b08b0',1,'speedBindings():&#160;MODE2.cpp'],['../_m_o_d_e3_8cpp.html#a942d4f1c3e151d6f264d0868766b08b0',1,'speedBindings():&#160;MODE3.cpp']]]
+];
