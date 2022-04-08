18ff99cd3b017b59b113d5fcca208bfa0744852d
AliceRivi14 <92019811+AliceRivi14@users.noreply.github.com>
2022-04-08 17:24:00 +0200

    Delete docs directory

--- docs/search/all_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['teleop_11',['Teleop',['../_m_o_d_e2_8cpp.html#afef03081108b5ffbbf11854a62083891',1,'Teleop():&#160;MODE2.cpp'],['../_m_o_d_e3_8cpp.html#afef03081108b5ffbbf11854a62083891',1,'Teleop():&#160;MODE3.cpp']]]
-];
