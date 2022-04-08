0bf370e0cf7811d26ddc4aa3cf8e42156bbf51db
AliceRivi14 <92019811+AliceRivi14@users.noreply.github.com>
2022-04-08 17:38:45 +0200

    Delete docs directory

--- docs/search/files_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['mode1_2ecpp_26',['MODE1.cpp',['../_m_o_d_e1_8cpp.html',1,'']]],
-  ['mode2_2ecpp_27',['MODE2.cpp',['../_m_o_d_e2_8cpp.html',1,'']]],
-  ['mode3_2ecpp_28',['MODE3.cpp',['../_m_o_d_e3_8cpp.html',1,'']]]
-];
