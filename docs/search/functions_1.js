9563db24ec699e06e0fe4db1c93fd3d37d2b1fc5
AliceRivi14 <92019811+AliceRivi14@users.noreply.github.com>
2022-04-19 09:31:25 +0200

    Delete docs directory

--- docs/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['key_31',['key',['../_m_o_d_e2_8cpp.html#a707311ac31b1d2e00c843eae201a6536',1,'key(&apos; &apos;):&#160;MODE2.cpp'],['../_m_o_d_e3_8cpp.html#a707311ac31b1d2e00c843eae201a6536',1,'key(&apos; &apos;):&#160;MODE3.cpp']]]
-];
--- docs/search/functions_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['x_40',['x',['../_m_o_d_e2_8cpp.html#a58456ccdd2b0d0e7ebb96c0a2ec7ec6f',1,'x(0):&#160;MODE2.cpp'],['../_m_o_d_e3_8cpp.html#a58456ccdd2b0d0e7ebb96c0a2ec7ec6f',1,'x(0):&#160;MODE3.cpp']]]
-];
